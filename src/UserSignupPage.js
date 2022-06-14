8619522be2005be860f7fc49eb2c7ee0155e2220
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-14 14:21:41 +0300

    make user sign up page basic page

--- src/UserSignupPage.js
+++ src/UserSignupPage.js
@@ -3,7 +3,26 @@ import React from 'react';
 class UserSignupPage extends React.Component{
     render(){
         return (
-           <h1>Sign Up</h1>
+            <form>
+                <h1>Sign Up</h1>
+                    <div>
+                    <h1><label>Username</label> </h1>
+                    <input></input>
+                    </div>
+                    <div>
+                    <h1><label>Display Name</label></h1>
+                    <input></input>
+                    </div>
+                    <div>
+                    <h1><label>Password</label></h1>
+                    <input type="password"></input>
+                    </div>
+                    <div>
+                    <h1><label>Password Repeat</label></h1>
+                    <input type="password"></input>
+                    </div>
+                    <button>Sign Up</button>
+            </form>
         );
     }
 }
