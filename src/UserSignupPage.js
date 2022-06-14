8c61afad761d9008c5eed9fcf4d27424fab0e748
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-14 15:07:36 +0300

    make user interactions

--- src/UserSignupPage.js
+++ src/UserSignupPage.js
@@ -1,25 +1,41 @@
 import React from 'react';
 
 class UserSignupPage extends React.Component{
+
+    state = {
+        username: null,
+        displayName: null,
+        password: null,
+        passwordRepeat: null
+    }
+
+    onChange = event => {
+        const value = event.target.value;
+        const field = event.target.name;
+        this.setState({
+            [field]: value
+        });
+    }
+
     render(){
         return (
             <form>
                 <h1>Sign Up</h1>
                     <div>
                     <h1><label>Username</label> </h1>
-                    <input></input>
+                    <input name="username" onChange={this.onChange}></input>
                     </div>
                     <div>
                     <h1><label>Display Name</label></h1>
-                    <input></input>
+                    <input name="displayName" onChange={this.onChange}></input>
                     </div>
                     <div>
                     <h1><label>Password</label></h1>
-                    <input type="password"></input>
+                    <input name="password" onChange={this.onChange} type="password"></input>
                     </div>
                     <div>
                     <h1><label>Password Repeat</label></h1>
-                    <input type="password"></input>
+                    <input name="passwordRepeat" onChange={this.onChange} type="password"></input>
                     </div>
                     <button>Sign Up</button>
             </form>
