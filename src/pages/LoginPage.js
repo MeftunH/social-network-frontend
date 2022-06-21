d2ab93e3072d1c7ca89f51a15be9fbb9d64c1df4
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-21 05:18:39 +0300

    set basic auth functionality on login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Input from '../components/Input'
 import { withTranslation } from 'react-i18next';
+import {login} from '../api/apiCalls'
 class LoginPage extends Component {
     state = {
         username: null,
@@ -12,6 +13,16 @@ class LoginPage extends Component {
     this.setState({ [name]: value });
     }
 
+    onClickLogin = event => {
+    event.preventDefault();
+    const {username,password} = this.state;
+    const creds = {
+        username,
+        password,
+    }
+    login(creds);
+    }
+
     render() {
         const { t } = this.props;
         return (
@@ -21,7 +32,7 @@ class LoginPage extends Component {
                     <Input label={t('Username')} name="username" onChange={this.onChange}></Input>
                     <Input label={t('Password')} name="password" onChange={this.onChange}></Input>
                     <div className='text-center'>
-                    <button className='btn btn-primary'>{t('Login')}</button>
+                    <button className='btn btn-primary' onClick={this.onClickLogin}>{t('Login')}</button>
                     </div>
                 </form>
             </div>
