29fcdd5cae99f7e85ee931abffe898ac0e538628
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-21 03:28:14 +0300

    add cursor style to flags

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -3,91 +3,128 @@ import { signup } from "../api/apiCalls";
 import Input from "../components/Input";
 import { withTranslation } from "react-i18next";
 class UserSignupPage extends React.Component {
-    state = {
-      username: null,
-      displayName: null,
-      password: null,
-      passwordRepeat: null,
-      pendingApiCall: false,
-      errors: {}
-    };
-  
-    onChange = event => {
-      const {t} = this.props;
-      const { name, value } = event.target;
-      const errors = { ...this.state.errors };
-      errors[name] = undefined;
+  state = {
+    username: null,
+    displayName: null,
+    password: null,
+    passwordRepeat: null,
+    pendingApiCall: false,
+    errors: {},
+  };
 
-      if(name === 'password' || name === 'passwordRepeat') {
-      if(name === 'password' && value !== this.state.passwordRepeat) {
-        errors.passwordRepeat = t('Passwords do not match');
-      } else if(name === 'passwordRepeat' && value !== this.state.password) {
-        errors.passwordRepeat = t('Passwords do not match');
+  onChange = (event) => {
+    const { t } = this.props;
+    const { name, value } = event.target;
+    const errors = { ...this.state.errors };
+    errors[name] = undefined;
+
+    if (name === "password" || name === "passwordRepeat") {
+      if (name === "password" && value !== this.state.passwordRepeat) {
+        errors.passwordRepeat = t("Passwords do not match");
+      } else if (name === "passwordRepeat" && value !== this.state.password) {
+        errors.passwordRepeat = t("Passwords do not match");
       } else {
         errors.passwordRepeat = undefined;
       }
     }
 
-      this.setState({
-        [name]: value,
-        errors
-      });
-    };
-  
-    onClickSignup = async event => {
-      event.preventDefault();
-  
-      const { username, displayName, password } = this.state;
-  
-      const body = {
-        username,
-        displayName,
-        password
-      };
-      this.setState({ pendingApiCall: true });
-  
-      try {
-        const response = await signup(body);
-      } catch (error) {
-        if (error.response.data.validationErrors) {
-          this.setState({ errors: error.response.data.validationErrors });
-        }
-      }
-  
-      this.setState({ pendingApiCall: false });
-    };
+    this.setState({
+      [name]: value,
+      errors,
+    });
+  };
 
-    onChangeLanguage = language => {
-        const { i18n } = this.props;
-        i18n.changeLanguage(language);
+  onClickSignup = async (event) => {
+    event.preventDefault();
+
+    const { username, displayName, password } = this.state;
+
+    const body = {
+      username,
+      displayName,
+      password,
+    };
+    this.setState({ pendingApiCall: true });
+
+    try {
+      const response = await signup(body);
+    } catch (error) {
+      if (error.response.data.validationErrors) {
+        this.setState({ errors: error.response.data.validationErrors });
+      }
     }
-  
-    render() {
-      const { pendingApiCall, errors } = this.state;
-      const { username,displayName,password,passwordRepeat } = errors;
-      const {t} = this.props;
-      return (
-        <div className="container">
-          <form>
-            <h1 className="text-center">{t('Sign up')}</h1>
-            <Input name="username" label={t("Username")} error={username} onChange={this.onChange}></Input>
-            <Input name="displayName" label={t("Display Name")} error={displayName} onChange={this.onChange}></Input>
-            <Input name="password" label={t("Password")} error={password} onChange={this.onChange} type="password"></Input>
-            <Input name="passwordRepeat" label={t("Password Repeat")} error={passwordRepeat} onChange={this.onChange} type="password"></Input>
-          
-            <div className="text-center">
-              <button className="btn btn-primary" onClick={this.onClickSignup} disabled={pendingApiCall || passwordRepeat !== undefined}>
-                {pendingApiCall && <span className="spinner-border spinner-border-sm"></span>} {t('Sign up')}
-              </button>
-            </div>
-            <div>
-            <img src="https://flagcdn.com/24x18/gb-eng.png" onClick={()=>this.onChangeLanguage('en')}></img>
-            <img src="https://flagcdn.com/24x18/tr.png"  style={{paddingLeft: '5px'}} onClick={()=>this.onChangeLanguage('tr')}></img>
-            </div>
-          </form>
-        </div>
-      );
-    }
+
+    this.setState({ pendingApiCall: false });
+  };
+
+  onChangeLanguage = (language) => {
+    const { i18n } = this.props;
+    i18n.changeLanguage(language);
+  };
+
+  render() {
+    const { pendingApiCall, errors } = this.state;
+    const { username, displayName, password, passwordRepeat } = errors;
+    const { t } = this.props;
+    return (
+      <div className="container">
+        <form>
+          <h1 className="text-center">{t("Sign up")}</h1>
+          <Input
+            name="username"
+            label={t("Username")}
+            error={username}
+            onChange={this.onChange}
+          ></Input>
+          <Input
+            name="displayName"
+            label={t("Display Name")}
+            error={displayName}
+            onChange={this.onChange}
+          ></Input>
+          <Input
+            name="password"
+            label={t("Password")}
+            error={password}
+            onChange={this.onChange}
+            type="password"
+          ></Input>
+          <Input
+            name="passwordRepeat"
+            label={t("Password Repeat")}
+            error={passwordRepeat}
+            onChange={this.onChange}
+            type="password"
+          ></Input>
+
+          <div className="text-center">
+            <button
+              className="btn btn-primary"
+              onClick={this.onClickSignup}
+              disabled={pendingApiCall || passwordRepeat !== undefined}
+            >
+              {pendingApiCall && (
+                <span className="spinner-border spinner-border-sm"></span>
+              )}{" "}
+              {t("Sign up")}
+            </button>
+          </div>
+          <div>
+            <img
+              src="https://flagcdn.com/24x18/gb-eng.png"
+              onClick={() => this.onChangeLanguage("en")}
+              style={{ cursor: "pointer" }}
+            ></img>
+            <img
+              src="https://flagcdn.com/24x18/tr.png"
+              style={{ paddingLeft: "5px", cursor: "pointer" }}
+              onClick={() => this.onChangeLanguage("tr")}
+            ></img>
+          </div>
+        </form>
+      </div>
+    );
   }
-  
-  export default withTranslation()(UserSignupPage);
+}
+
+export default withTranslation()(UserSignupPage);
