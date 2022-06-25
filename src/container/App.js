dd7f5b8e1b6395497115e09eb1be41456383d034
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-26 02:39:20 +0300

    set router

--- src/container/App.js
+++ src/container/App.js
@@ -4,10 +4,19 @@ import LanguageSelector from "../components/LanguageSelector";
 import ApiProgress from "../shared/ApiProgress";
 import HomePage from "../pages/HomePage";
 import UserPage from "../pages/UserPage";
+import {HashRouter,Redirect,Route,Switch} from 'react-router-dom'
 function App() {
   return (
-    <div className="row">
-      <UserPage></UserPage>
+    <div>
+      <HashRouter>
+      <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/login" exact component={LoginPage} />
+      <Route path="/signup" exact component={UserSignupPage} />
+      <Route path="/user/:username" exact component={UserPage} />
+      <Redirect to="/" />
+      </Switch>
+      </HashRouter>
       <LanguageSelector />
     </div>
   );
