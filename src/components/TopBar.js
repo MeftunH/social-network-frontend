d19186e2a68981a3d03350074a1a214800b0b5a9
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-26 02:52:45 +0300

    set link instead of a tag

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import logo from '../assets/logo.png';
+import { Link } from "react-router-dom";
 
 class TopBar extends Component {
   render() {
     return (
       <div className="shadow-sm  bg-light mb-2">
         <nav className="navbar navbar-light bg-light container">
-          <a className="navbar-brand" href="#/">
+          <Link className="navbar-brand" to="/">
            <img src={logo} width="60" alt="SocialApp Logo"/>
             SocialApp
-          </a>
+          </Link>
         </nav>
       </div>
     );
