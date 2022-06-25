449c1ad49155d645a4be517d8800c79ae6690588
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-26 00:12:40 +0300

    api progress clean up

--- src/index.js
+++ src/index.js
@@ -2,21 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './bootstrap-override.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import UserSignupPage from './pages/UserSignupPage';
-import LoginPage from './pages/LoginPage';
 import './i18n';
-import LanguageSelector from './components/LanguageSelector';
-import ApiProgress from './shared/ApiProgress';
+import App from './container/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ApiProgress>
-    <LoginPage />
-    </ApiProgress>
-    <LanguageSelector />
+  <App></App>
   </React.StrictMode>
 );
 
