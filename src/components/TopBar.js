a1b5b92cf2814eb1e733bdfb8e28901888ac7f8b
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-30 22:16:29 +0300

    set topbar change to when user logged in

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,31 +1,55 @@
 import React, { Component } from "react";
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
-import {withTranslation} from "react-i18next";
+import { withTranslation } from "react-i18next";
 class TopBar extends Component {
+  state = {
+    isLoggedIn: true,
+    username: "user1",
+  };
+
   render() {
     const { t } = this.props;
+    const { isLoggedIn, username } = this.state;
+
+    let links = (
+      <ul className="navbar-nav ml-auto">
+        <li>
+          <Link className="nav-link" to="/login">
+            {t("Login")}
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/signup">
+            {t("Sign up")}
+          </Link>
+        </li>
+      </ul>
+    );
+
+    if (isLoggedIn) {
+      links = (
+        <ul className="navbar-nav ml-auto">
+          <li>
+            <li>
+              <Link className="nav-link" to={`/user/${username}`}>
+                {username}
+              </Link>
+            </li>
+            <li className="nav-link">{t("Logout")}</li>
+          </li>
+        </ul>
+      );
+    }
     return (
       <div className="shadow-sm  bg-light mb-2">
         <nav className="navbar navbar-light bg-light container navbar-expand">
           <Link className="navbar-brand" to="/">
-           <img src={logo} width="60" alt="SocialApp Logo"/>
+            <img src={logo} width="60" alt="SocialApp Logo" />
             SocialApp
           </Link>
-       
-         <ul className="navbar-nav ml-auto">
-         <li>
-         <Link className="nav-link" to='/login'>
-         {t('Login')}
-         </Link>
-         </li>
-         <li>
-         <Link className="nav-link" to='/signup'>
-         {t('Sign up')}
-         </Link>
-         </li>
-         </ul>
 
+          {links}
         </nav>
       </div>
     );
