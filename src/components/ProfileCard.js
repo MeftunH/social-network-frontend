07f883a26792f688b80e1ecafb5f08ba46d62a0e
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-03 01:46:48 +0300

    profile card context bug fix

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { Authentication } from "../shared/AuthenticationContext";
 
 const ProfileCard = (props) => {
-    const pathUsername = props.match.params.username;
-    const loggedInUsername = props.username;
-    let message = "We cannot edit"
-    if (pathUsername === loggedInUsername) {
-        message = "Edit your profile"
-    }
-    return (
-        <div>
-            {message}
-        </div>
-    );
+  return (
+    <Authentication.Consumer>
+      {(value) => {
+        const pathUsername = props.match.params.username;
+        const loggedInUsername = value.state.username;
+        let message = "We cannot edit";
+        if (pathUsername === loggedInUsername) {
+          message = "Edit your profile";
+        }
+        return <div>{message}</div>;
+      }}
+    </Authentication.Consumer>
+  );
 };
 
 export default withRouter(ProfileCard);
