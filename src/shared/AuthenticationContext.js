04960dfe9bca1d4e47dead09b998247e6836dab3
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-02 04:57:43 +0300

    implement auth context

--- src/shared/AuthenticationContext.js
+++ src/shared/AuthenticationContext.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export const Authentication = React.createContext();
 
- class AuthenticationContext extends Component {
-    state = {
-        isLoggedIn: false,
-        username: "user1",
-      };
-    
-      onLoginSuccess = (username) => {
-        this.setState({ isLoggedIn: true, username });
-      };
-    
-      onLogoutSuccess = () => {
-        this.setState({ isLoggedIn: false, username: undefined });
-      };
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+class AuthenticationContext extends Component {
+  state = {
+    isLoggedIn: false,
+    username: undefined,
+    displayName: undefined,
+    image: undefined,
+    password: undefined,
+  };
+
+  onLoginSuccess = (authState) => {
+    this.setState({ isLoggedIn: true, ...authState });
+  };
+
+  onLogoutSuccess = () => {
+    this.setState({ isLoggedIn: false, username: undefined });
+  };
+  render() {
+    return (
+      <Authentication.Provider
+        value={{
+          state: { ...this.state },
+          onLoginSuccess: this.onLoginSuccess,
+          onLogoutSuccess: this.onLogoutSuccess,
+        }}
+      >
+        {this.props.children}
+      </Authentication.Provider>
+    );
+  }
 }
 export default AuthenticationContext;
