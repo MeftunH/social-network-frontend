0337749f1beb55ed79684cf7560f6e1b30fcac44
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-02 05:06:35 +0300

    fix component import bug

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./bootstrap-override.scss";
 import reportWebVitals from "./reportWebVitals";
 import "./i18n";
 import App from "./container/App";
-import { AuthenticationContext } from "./shared/AuthenticationContext";
+import AuthenticationContext from "./shared/AuthenticationContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
