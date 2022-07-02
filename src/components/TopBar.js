5ca0b9111120349e99344416c61ff40d6ffccdf9
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-02 05:06:51 +0300

    set auth consumer

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,52 +2,64 @@ import React, { Component } from "react";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 import { withTranslation } from "react-i18next";
+import { Authentication } from "./../shared/AuthenticationContext";
 class TopBar extends Component {
-
   render() {
- 
-    const { t,isLoggedIn,onLogoutSuccess, username } = this.props;
-
-    let links = (
-      <ul className="navbar-nav ml-auto">
-        <li>
-          <Link className="nav-link" to="/login">
-            {t("Login")}
-          </Link>
-        </li>
-        <li>
-          <Link className="nav-link" to="/signup">
-            {t("Sign up")}
-          </Link>
-        </li>
-      </ul>
-    );
-
-    if (isLoggedIn) {
-      links = (
-        <ul className="navbar-nav ml-auto">
-          <li>
-            <li>
-              <Link className="nav-link" to={`/user/${username}`}>
-                {username}
-              </Link>
-            </li>
-            <li className="nav-link" onClick={onLogoutSuccess} style={{cursor:'pointer'}}>{t("Logout")}</li>
-          </li>
-        </ul>
-      );
-    }
+    const { t } = this.props;
     return (
-      <div className="shadow-sm  bg-light mb-2">
-        <nav className="navbar navbar-light bg-light container navbar-expand">
-          <Link className="navbar-brand" to="/">
-            <img src={logo} width="60" alt="SocialApp Logo" />
-            SocialApp
-          </Link>
+      <Authentication.Consumer>
+        {(value) => {
+          const { state,onLogoutSuccess } = value;
+          const { isLoggedIn, username } = state;
+          let links = (
+            <ul className="navbar-nav ml-auto">
+              <li>
+                <Link className="nav-link" to="/login">
+                  {t("Login")}
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/signup">
+                  {t("Sign up")}
+                </Link>
+              </li>
+            </ul>
+          );
 
-          {links}
-        </nav>
-      </div>
+          if (isLoggedIn) {
+            links = (
+              <ul className="navbar-nav ml-auto">
+                <li>
+                  <li>
+                    <Link className="nav-link" to={`/user/${username}`}>
+                      {username}
+                    </Link>
+                  </li>
+                  <li
+                    className="nav-link"
+                    onClick={onLogoutSuccess}
+                    style={{ cursor: "pointer" }}
+                  >
+                    {t("Logout")}
+                  </li>
+                </li>
+              </ul>
+            );
+          }
+          return (
+            <div className="shadow-sm  bg-light mb-2">
+              <nav className="navbar navbar-light bg-light container navbar-expand">
+                <Link className="navbar-brand" to="/">
+                  <img src={logo} width="60" alt="SocialApp Logo" />
+                  SocialApp
+                </Link>
+
+                {links}
+              </nav>
+            </div>
+          );
+        }}
+      </Authentication.Consumer>
     );
   }
 }
