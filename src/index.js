1da56bc099379fc5feab83e67445e8ba81038544
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-03 23:53:28 +0300

    set default state

--- src/index.js
+++ src/index.js
@@ -6,28 +6,38 @@ import reportWebVitals from "./reportWebVitals";
 import "./i18n";
 import App from "./container/App";
 import { Provider } from "react-redux";
-import { createStore} from "redux";
+import { createStore } from "redux";
 // import AuthenticationContext from "./shared/AuthenticationContext";
 
+const loggedInState = {
+  isLoggedIn: true,
+  username: "user1",
+  displayName: "user1_dn",
+  image: null,
+  password: "P4ssword*",
+};
 
-const loggedInState =  {
-    isLoggedIn: true,
-    username: "user1",
-    displayName: "user1_dn",
-    image: null,
-    password: "P4ssword*",
-  };
+const defaultState = {
+  isLoggedIn: false,
+  username: undefined,
+  displayName: undefined,
+  image: undefined,
+  password: undefined,
+};
 
 //give last state and action to reducer
-const reducer = (state,reducer) => {
- return state;
-}
-const store = createStore(reducer,loggedInState);
+const reducer = (state, action) => {
+  if (action.type === "LOGOUT_SUCCESS") {
+    return defaultState;
+  }
+  return state;
+};
+const store = createStore(reducer, loggedInState);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
-   </Provider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
