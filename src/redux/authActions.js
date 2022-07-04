67bd0ef1013b07abfd90507127d309418aae1255
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-05 01:47:34 +0300

    add login success

--- src/redux/authActions.js
+++ src/redux/authActions.js
@@ -3,3 +3,10 @@ export const logoutSuccess = () => {
     type: "LOGOUT_SUCCESS",
  }
 }
+
+export const loginSuccess = (authState) => {
+    return {
+        type: "LOGIN_SUCCESS",
+        payload: authState,
+    }
+}
