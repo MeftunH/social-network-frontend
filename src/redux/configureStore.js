fa348b7a77ee2dfa663f4ac72ceefe2a1608b6ac
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-05 05:08:37 +0300

    set state tracking by storage

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -12,7 +12,13 @@ const loggedInState = {
 const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
   
 const configureStore = () => {
- return createStore(authReducer, loggedInState,devtools);  
+ 
+  const store =  createStore(authReducer, loggedInState,devtools);  
+  //calls on all state changes
+  store.subscribe(() => {
+   localStorage.setItem('SOCIAL_APP_AUTH',JSON.stringify(store.getState()));
+  });
+  return store;
 }
 
 export default configureStore;
